b4930ad735aaed684a625a63f0bc2faf4fbc1db2
Nicholas Rejack <nrejack@ufl.edu>
2017-12-21 21:21:44 -0500

    Simplified logging to eliminate explicit handlers.

--- weather.py
+++ weather.py
@@ -8,38 +8,16 @@ import logging
 
 def main():
     """ Fetch the weather. """
-    verbose = True
-    logger = configure_logging(verbose)
-    logger.info("Starting weather")
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', filename='weather.log', level=logging.DEBUG)
+    logging.info("Starting weather")
     gnv_forecast_url = 'http://api.wunderground.com/api/f77ce6503c744246/forecast10day/q/FL/Gainesville.json'
-    logger.info("Fetching Gainesville weather.")
+    logging.info("Fetching Gainesville weather.")
     gnv_forecast = requests.get(gnv_forecast_url)
-    logger.debug("Response: {}".format(gnv_forecast.status_code))
+    logging.debug("Response: {}".format(gnv_forecast.status_code))
     gnv_forecast_text = json.loads(gnv_forecast.text)
-    pp.pprint(gnv_forecast_text)
+    #pp.pprint(gnv_forecast_text)
 
-def configure_logging(verbose):
-    """Configure the logger."""
-    # set up logging
-    logger = logging.getLogger()
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    logger.setLevel(logging.DEBUG)
-    # logging to file
-    fh = logging.FileHandler('weather.log')
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
 
-    # logging to console
-    ch = logging.StreamHandler()
-    if verbose:
-        ch.setLevel(logging.DEBUG)
-    else:
-        ch.setLevel(logging.WARNING)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    return logger
 
 if __name__ == '__main__':
     main()
